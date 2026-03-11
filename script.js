1bda85eb569418d3460ab32f7e62fab457950eea
KhoirulArifPratama <khoirularif932@gmail.com>
2026-03-11 16:37:17 +0700

    Pertemuan 4

---
+++ script.js
@@ -0,0 +1,18 @@
+// Date() untuk menampilkan tanggal saat ini [cite: 195]
+Date()
+
+// Mendapatkan tanggal hari ini [cite: 197]
+let tanggal = new Date()
+
+// Menampilkan ke elemen id "tanggal" [cite: 198]
+document.getElementById("tanggal").innerHTML = tanggal
+
+// Fungsi alert tampil() [cite: 199]
+function tampil() {
+    alert("Ini adalah button")
+}
+
+// Fungsi alert show() [cite: 203]
+function show() {
+    alert("ini adalah pop up untuk tulisan perkenalan")
+}
